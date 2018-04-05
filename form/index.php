3d63982cb240b00d90bcfe5efb7d0b0efdb3a9d1
Resident234 <FqjJrDCxSG8>
2018-04-05 23:58:15 +0900

    форма . конечный

---
+++ form/index.php
@@ -0,0 +1,30 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Заполнение формы");
+?>
+
+	<?$APPLICATION->IncludeComponent(
+		"aspro:form.stroy", "contacts",
+		Array(
+			"IBLOCK_TYPE" => "aspro_stroy_form",
+			"IBLOCK_ID" => 4,
+			"USE_CAPTCHA" => "N",
+			"AJAX_MODE" => "Y",
+			"AJAX_OPTION_JUMP" => "Y",
+			"AJAX_OPTION_STYLE" => "Y",
+			"AJAX_OPTION_HISTORY" => "N",
+			"CACHE_TYPE" => "A",
+			"CACHE_TIME" => "100000",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			//"IS_PLACEHOLDER" => "Y",
+			"SUCCESS_MESSAGE" => "Форма успешно отправлена",
+			"SEND_BUTTON_NAME" => "Отправить",
+			"SEND_BUTTON_CLASS" => "btn btn-default",
+			"DISPLAY_CLOSE_BUTTON" => "Y",
+			"CLOSE_BUTTON_NAME" => "Закрыть",
+			"CLOSE_BUTTON_CLASS" => "jqmClose btn btn-default bottom-close",
+		)
+	);?>
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- html-markup/src/scripts/app.js
+++ html-markup/src/scripts/app.js
@@ -1,3 +1,4 @@
 //= ../bower_components/modernizr.js
 //= ../bower_components/jquery/dist/jquery.min.js
 //= ../bower_components/jquery-ui/jquery-ui.min.js
+//= ../bower_components/jquery.inputmask.bundle.min.js
