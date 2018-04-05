2c53cda8eedc718cb5851de98106d8628b61aeeb
Resident234 <FqjJrDCxSG8>
2018-04-05 21:37:03 +0900

    Если мы нажимаем на элементы пагинации - список элементов заменяется на список конкретной страницы (так же по AJAX)

--- html-markup/src/scripts/parts/default.js
+++ html-markup/src/scripts/parts/default.js
@@ -6,10 +6,14 @@ $(function () {
         var lastPoint = 0;
         var paginationContainerForTrigger = $('.js-pagination-container');
         var link = paginationContainerForTrigger.find('.ajax-link');
+        var linkGlobal = link;
+        var paginationWrap = $(".js-pagination-wrap");
+        var page = link.data('page');
+        var countPages = paginationContainerForTrigger.data('count-pages');
 
         $('body').on('click', '.ajax-link', function (e) {
             e.preventDefault();
-            ajaxLoadContent($(this));
+            ajaxLoadContent($(this), "append");
         });
 
         $(document).scroll(function () {
@@ -29,35 +33,61 @@ $(function () {
                 /** когда кнопка окажется на трети видимой части экрана,
                  * считая снизу, то сработает догрузка */
                 if ($('.ajax-link').length) {
-                    ajaxLoadContent(link);
+                    ajaxLoadContent(link, "append");
                 }
             }
 
         });
 
-        function ajaxLoadContent($link){
+        $(document).on("click", ".js-pagination-wrap a", function (event) {
+            var a = $(this);
+            page = a.attr('data-page');
+            ajaxLoadContent(a, "html");
+            return false;
+        });
+
+        function ajaxLoadContent($link, type){
             var link = $link;
-            var strTitleNormal = link.data("title-normal");
-            var strTitleLoading = link.data("title-loading");
+            var strTitleNormal = linkGlobal.data("title-normal");
+            var strTitleLoading = linkGlobal.data("title-loading");
 
             pageIsLoading = true;
             $(".js-preloader-Wait").show();
-            link.html(strTitleLoading);
+            linkGlobal.html(strTitleLoading);
 
             $.get(link.attr('href'), function (data) {
-
                 $(".js-preloader-Wait").hide();
-                link.html(strTitleNormal);
+                linkGlobal.html(strTitleNormal);
                 pageIsLoading = false;
 
+
+                paginationWrap.find(".btn.btn_green-border").removeClass("btn_green-border").addClass("btn_green");
+                paginationWrap.find(".btn[data-page='" + page + "']").removeClass("btn_green").addClass("btn_green-border");
+
                 var response = $(data);
-                $('.ajax-list').append(response.find('.ajax-list').html());
+                if (type == "append") {
+                    $('.ajax-list').append(response.find('.ajax-list').html());
+                } else {
+                    alert(page + " - " + countPages);
+
+                    $('.ajax-list').html(response.find('.ajax-list').html());
+                    if (page < countPages) {
+                        linkGlobal.show();
+                    }
+                }
                 if (response.find('.ajax-link').length) {
-                    link.attr('href', response.find('.ajax-link').attr('href'));
+                    linkGlobal.attr('href', response.find('.ajax-link').attr('href'));
+                    page = Number(page) + 1;
+                    linkGlobal.attr("data-page", String(page));//parseInt(page) + 1
+
                 }
                 else {
-                    link.remove();
+                    //link.remove();
+                    linkGlobal.hide();
                 }
+
+
+
             });
         }
 
--- local/templates/.default/components/bitrix/system.pagenavigation/ajax/template.php
+++ local/templates/.default/components/bitrix/system.pagenavigation/ajax/template.php
@@ -7,18 +7,90 @@ $strNavQueryStringFull = ($arResult["NavQueryString"] != "" ? "?".$arResult["Nav
 if($arResult["NavPageCount"] > 1)
 {
 ?>
-<div class="news-wrap__button-wrap news-wrap__button-show-more js-pagination-container">
-<?
-	$bFirst = true;
+    <div class="news-wrap__pagination-container js-pagination-container" data-count-pages="<?=$arResult["NavPageCount"];?>">
+        <div class="news-wrap__button-wrap news-wrap__button-show-more">
+            <?
+            $bFirst = true;
 
-	if($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
-?>
-    <a href="<?=$arResult["sUrlPath"]?>?<?=$strNavQueryString?>PAGEN_<?=$arResult["NavNum"]?>=<?=($arResult["NavPageNomer"]+1)?>" class="btn ajax-link btn_green-border-center"
-    data-title-normal="Показать ещё" data-title-loading="Загружаем...">Показать ещё</a>
-<?
-	endif;
-?>
-</div>
+            if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
+                ?>
+                <a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= ($arResult["NavPageNomer"] + 1) ?>"
+                   class="btn ajax-link btn_green-border-center"
+                   data-title-normal="Показать ещё" data-title-loading="Загружаем..." data-page="<?= ($arResult["NavPageNomer"] + 1) ?>">Показать ещё</a>
+                <?
+            endif;
+            ?>
+
+        </div>
+
+        <div class="news-wrap__pagination-wrap js-pagination-wrap">
+
+            <?
+
+            if ($arResult["NavPageNomer"] > 1):
+                if ($arResult["nStartPage"] > 1):
+                    $bFirst = false;
+                    if ($arResult["bSavePage"]):
+                        ?>
+                        <a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=1"
+                           class="btn" data-page="1">1</a>
+                        <?
+                    else:
+                        ?>
+                        <a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>"
+                           class="btn btn_green-border" data-page="1">1</a>
+                        <?
+                    endif;
+                    if ($arResult["nStartPage"] > 2):
+                        ?>
+                        <a class="btn btn_green">...</a>
+                        <?
+                    endif;
+                endif;
+            endif;
+
+            do {
+                if ($arResult["nStartPage"] == $arResult["NavPageNomer"]):
+                    ?>
+                    <a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["nStartPage"] ?>"
+                       class="btn btn_green-border" data-page="<?= $arResult["nStartPage"] ?>"><?= $arResult["nStartPage"] ?></a>
+                    <?
+                elseif ($arResult["nStartPage"] == 1 && $arResult["bSavePage"] == false):
+                    ?>
+                    <a href="<?= $arResult["sUrlPath"] ?><?= $strNavQueryStringFull ?>"
+                       class="btn btn_green" data-page="<?= $arResult["nStartPage"] ?>"><?= $arResult["nStartPage"] ?></a>
+                    <?
+                else:
+                    ?>
+                    <a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["nStartPage"] ?>"
+                       class="btn btn_green" data-page="<?= $arResult["nStartPage"] ?>"><?= $arResult["nStartPage"] ?></a>
+                    <?
+                endif;
+
+                $arResult["nStartPage"]++;
+                $bFirst = false;
+            } while ($arResult["nStartPage"] <= $arResult["nEndPage"]);
+
+            if ($arResult["NavPageNomer"] < $arResult["NavPageCount"]):
+                if ($arResult["nEndPage"] < $arResult["NavPageCount"]):
+                    if ($arResult["nEndPage"] < ($arResult["NavPageCount"] - 1)):
+                        ?>
+                        <a class="btn btn_green">...</a>
+                        <?
+                    endif;
+                    ?>
+                    <a href="<?= $arResult["sUrlPath"] ?>?<?= $strNavQueryString ?>PAGEN_<?= $arResult["NavNum"] ?>=<?= $arResult["NavPageCount"] ?>"
+                       class="btn btn_green" data-page="<?= $arResult["NavPageCount"] ?>"><?= $arResult["NavPageCount"] ?></a>
+                    <?
+                endif;
+            endif;
+            ?>
+
+
+        </div>
+
+    </div>
+
 <?
 }
 ?>
